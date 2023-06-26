7c282347d78d28f5d8df433272648ea89aadc8e5
tanvir <tzather@gmail.com>
2023-06-26 14:00:35 -0600

    create default page

---
+++ Api.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Zuhid.Identity.Api.UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+  [TestMethod]
+  public void TestMethod1()
+  {
+  }
+}
---
+++ Api/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapGet("/", (HttpResponse response) =>
+{
+  response.ContentType = "text/html";
+  return @"
+<html>
+<body style='padding:100px 0;text-align:center;font-size:xxx-large;'>
+    Api is running<br/><br/>
+    <a href='/swagger'>View Swagger</a>
+</body></html>";
+});
+app.MapControllers();
+
+app.Run();
